f250b1bae411cc3cada9437f4573fb8c0a07d6aa
Shakil Shahadat <shakil.regi@gmail.com>
2019-09-09 09:28:58 +0600

    Create 002.py

---
+++ 002.py
@@ -0,0 +1,5 @@
+firstname = input( 'Please enter your first name: ' )
+surname = input( 'Please enter your surname: ' )
+
+print( 'Hello', firstname, surname )
+# Python adds a space after each string part
