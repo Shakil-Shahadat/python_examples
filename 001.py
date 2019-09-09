f87bb88dbbcda58a5fb8310d739c085306d5b84f
Shakil Shahadat <shakil.regi@gmail.com>
2019-09-09 10:30:08 +0600

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,3 +1,4 @@
 firstname = input( 'Please enter your first name: ' )
 print( 'Hello', firstname )
+# Python 3 does not support print statement without parenthesis
 # Python automatically adds a space after the string
