b969c72fbd7103eb819a279aa771d9333abf5da5
Shakil Shahadat <shakil.regi@gmail.com>
2019-09-09 09:40:19 +0600

    Create 004.py

---
+++ 004.py
@@ -0,0 +1,5 @@
+num1 = int(input( 'Enter first number: ' ))
+num2 = int(input( 'Enter second number: ' ))
+# int() is required, otherwise the input is stored as a string
+
+print( 'The total is', num1 + num2 )
