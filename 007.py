dce1a5fe527f000b73d28299736ece442b0929a1
Shakil Shahadat <shakil.regi@gmail.com>
2019-09-10 10:26:54 +0600

    Create 007.py

---
+++ 007.py
@@ -0,0 +1,5 @@
+name = input( 'Your name: ' )
+age = int( input( 'Your age: ' ) )
+newage = age + 1
+print( name + ', next birthday you will be', newage )
+# comma will add a space, plus sign won't add anything
