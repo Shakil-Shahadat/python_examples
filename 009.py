68d7bca11e1af91ad83360482cc190b08ef80c2c
Shakil Shahadat <shakil.regi@gmail.com>
2019-09-10 10:50:36 +0600

    Create 009.py

---
+++ 009.py
@@ -0,0 +1,5 @@
+day = int( input( 'Number of days: ' ) )
+
+print( 'Number of hours in those days:', day * 24 )
+print( 'Number of minutes in those days:', day * 24 * 60 )
+print( 'Number of seconds in those days:', day * 24 * 60 * 60 )
