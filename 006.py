aeae890177b6223062ad0944dee3f3b49b85f6c6
Shakil Shahadat <shakil.regi@gmail.com>
2019-09-10 08:04:00 +0600

    Create 006.py

---
+++ 006.py
@@ -0,0 +1,5 @@
+slice = int( input( 'Enter the number of slices: ' ) )
+eaten = int( input( 'Enter the number of slices you have eaten: ' ) )
+left = slice - eaten
+print( 'Number of slices left:', left )
+# '+' can't be used to concatenate string and int
