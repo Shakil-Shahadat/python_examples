966fda1e800e2d87a84ad92176a52fe6140910a7
Shakil Shahadat <shakil.regi@gmail.com>
2019-09-15 12:56:54 +0600

    Create 011.py

---
+++ 011.py
@@ -0,0 +1,4 @@
+larger = int( input( 'Enter a number over 100: ' ) )
+smaller = int( input( 'Enter a number under 10: ' ) )
+
+print( larger, 'goes into', smaller, ',', larger // smaller, 'times.' )
