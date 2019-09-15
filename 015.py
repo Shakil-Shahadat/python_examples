badded1b2901969be53ad8834b2450858b24e867
Shakil Shahadat <shakil.regi@gmail.com>
2019-09-15 13:28:32 +0600

    Create 015.py

---
+++ 015.py
@@ -0,0 +1,6 @@
+color = input( 'Enter your favorite color: ' )
+
+if color == 'red' or color == 'RED' or color == 'Red':
+    print( 'I like red too' )
+else:
+    print( 'I don’t like ' + color + ', I prefer red' )
