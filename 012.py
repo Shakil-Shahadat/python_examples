7eaaf679bcdc869d144e16a12aec13d3c42ca052
Shakil Shahadat <shakil.regi@gmail.com>
2019-09-15 12:57:07 +0600

    Create 012.py

---
+++ 012.py
@@ -0,0 +1,9 @@
+first = int( input( 'Enter first number: ' ) )
+second = int( input( 'Enter second number: ' ) )
+
+if first > second:
+    print( 'The second number is', second )
+    print( 'The first number is', first )
+else:
+    print( 'The first number is', first )
+    print( 'The second number is', second )
